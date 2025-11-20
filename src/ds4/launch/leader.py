350c968dd3b4b49933499bc88742a67dbad06079
Tobias Thorgren <thorgrentobias@gmail.com>
2025-11-20 14:49:57 +0100

    launch

---
+++ src/ds4/launch/leader.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='joy',
+            executable='joy_node', 
+            name='joy_node',
+            parameters=[{
+                'dev': '/dev/input/js0',
+                'deadzone': 0.05
+            }]                  
+        ),
+        Node(
+            package='ds4',
+            executable='ds4_print', 
+            name='ds4_print',
+            parameters=[{
+            }]
+        ),
+        
+    ])
