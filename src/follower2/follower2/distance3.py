052e37f66643b64197acf6d8436d0f2e9019bea4
Tobias Thorgren <thorgrentobias@gmail.com>
2025-12-11 14:42:10 +0100

    distance3

--- src/follower2/follower2/distance3.py
+++ src/follower2/follower2/distance3.py
@@ -16,12 +16,12 @@ class MultiUltrasonicNode(Node):
         self.gpio_echo_c = 6
         
         # 2. Left Sensor (NEW - UPDATE THESE PINS)
-        self.gpio_trig_l = 17 
-        self.gpio_echo_l = 27
+        self.gpio_trig_l = 22 
+        self.gpio_echo_l = 23
         
         # 3. Right Sensor (NEW - UPDATE THESE PINS)
-        self.gpio_trig_r = 22
-        self.gpio_echo_r = 23
+        self.gpio_trig_r = 17
+        self.gpio_echo_r = 27
 
         self.min_range = 0.02
         self.max_range = 4.0
