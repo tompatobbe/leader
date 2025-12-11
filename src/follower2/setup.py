abd9fa34282447b70589cb3a2856ef4085bf0d77
Tobias Thorgren <thorgrentobias@gmail.com>
2025-12-11 14:21:14 +0100

    launch file setup for follower2

--- src/follower2/setup.py
+++ src/follower2/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/follower2.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
